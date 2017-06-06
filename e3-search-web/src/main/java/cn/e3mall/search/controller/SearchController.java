187e6a4e81d185df0aa557c917c2e4a8b5a28c09
zxy <1102610788@qq.com>
2017-06-06 19:58:12 +0800

    搜索系统的添加

---
+++ e3-search-web/src/main/java/cn/e3mall/search/controller/SearchController.java
@@ -0,0 +1,22 @@
+package cn.e3mall.search.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 商品查询controller
+ * @author Administrator
+ *
+ */
+@Controller
+public class SearchController {
+	
+	
+	
+	//根据关键词进行查询
+	@RequestMapping("/search")
+	public void test(){
+		
+	}
+	
+}
